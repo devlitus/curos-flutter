57b759e2f8837de1c38be7c2655a9ac445423786
devlitus <developercarles@gmail.com>
2020-06-29 10:19:22 +0200

    diseños de pantalla

---
+++ disenos/android/app/src/main/kotlin/com/ejemplos/disenos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ejemplos.disenos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ disenos/lib/src/pages/scroll_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Scrollpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          _pagina1(),
+          _pagina2(),
+        ],
+      ),
+    );
+  }
+
+  Widget _pagina1() {
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _pagina2() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+      child: Center(
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.blue,
+          textColor: Colors.white,
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: Text(
+              'Bienvenido',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image(
+          image: AssetImage('assets/original.png'),
+          fit: BoxFit.cover,
+        ));
+  }
+
+  Widget _textos() {
+    final stiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            '11º',
+            style: stiloTexto,
+          ),
+          Text(
+            'Miercoles',
+            style: stiloTexto,
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 70.0,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
